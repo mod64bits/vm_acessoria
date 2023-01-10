5e79ecb197f1941289e9c32d716a7796f1d991da
mod64bits <mod64bits@gmail.com>
2023-01-10 20:30:34 -0300

    model servicos

--- apps/servicos/models.py
+++ apps/servicos/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from apps.core.models import Base
 
-# Create your models here.
+
+class TipoDeServico(Base):
+    pass
+
+
+class Servico(models.Model):
+    tipo_servico = models.ForeignKey(TipoDeServico, on_delete=models.CASCADE)
+    descricao = models.TextField('Descrição')
+    created = models.DateTimeField('Criado em', auto_now_add=True)
+    modified = models.DateTimeField('Modificado em', auto_now=True)
+
