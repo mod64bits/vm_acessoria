2319a2229c16918331006d27785fee31cb00d863
mod64bits <mod64bits@gmail.com>
2023-01-11 01:42:44 -0300

    fim base

--- apps/clientes/models.py
+++ apps/clientes/models.py
@@ -3,7 +3,7 @@ from apps.core.models import Base
 
 
 class Cliente(Base):
-    documento = models.CharField('CPF/CNPJ', max_length=50, unique=True)
+    documento = models.CharField('CPF/CNPJ', max_length=50, unique=True, null=True, blank=True)
     email = models.EmailField('E-mail', null=True, blank=True)
 
     class Meta:
